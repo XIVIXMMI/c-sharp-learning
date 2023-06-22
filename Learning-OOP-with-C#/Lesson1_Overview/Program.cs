d9918499e1a3c60b54af3bd9d736b5d0e5fef9b5
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-06-22 23:44:12 +0700

    Learning OOP with C# Lesson 1 2

---
+++ Learning-OOP-with-C#/Lesson1_Overview/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+    Lập trình hướng đối tượng là phương pháp lập trình lấy đối tượng làm nền tảng
+    để xây dựng chương trình.
+    Là phương pháp lập trình dựa trên kiến trúc lớp(class) và đối tượng(object)
+    
+        Một đối tương bao gồm 2 thông tin:
+    ?Thuộc tính và phương thức
+    *Thuộc tính chính là những thông tin, đặc điểm của đối tượng
+    Ví dụ một người sẽ có họ tên, màu da, kiểu tóc...
+    *Phương thức là những thao tác, hàng động mà đối tượng đó có thể thực hiện.
+    Ví dụ một người có thẻ thực hiện các hành động ăn, uống, đi, đứng...
+
+        Lớp: 
+    * Các đối tượng có các đặc tính tương tự nhau được gom lại thành 1 lớp đối tượng
+    * Bên trong lớp cũng có 2 thành phần chính là thuộc tính và phương thức
+    * Ngoài ra, lớp còn được dùng để định nghĩa ra kiểu dữ liệu mới.
+
+    ?Sự khác nhau giữa đối tượng và lớp:
+    Lớp là một khuôn mẫu còn đối tượng là một thể hiện cụ thể dựa trên khuôn mẫu đó
+    
+    4 đặc điểm chính của OOP
+    *Tính đóng gói:
+    Các dữ liệu và phương thức có liên quan với nhau được đóng gói thành các lớp để tiện cho việc quản lý và sử dụng.
+    Ngoài ra, đóng gói còn để che giấu một số thông tin và chi tiết cài đặt nội bộ bên ngoài không thể nhìn thấy.
+    *Tính trừu tượng: 
+    Khi viết chương trình theo phong cách hướng đối tượng, việc thiết kế các đối tượng 
+    ta cần rút tỉa ra những đặc trưng chung của chúng rồi trừu tượng thành các interface và thiết kế xem chúng sẽ tương tác với nhau như thế nào.
+    *Tính thừa kế:
+    Lớp cha có thể chia sẻ dữ liệu và phương thức cho các lớp con, các lớp con khỏi phải định nghĩa lại, giúp chương trình ngắn gọn
+    *Tính đa hình:
+    Là hiện tượng các đối tượng thuộc các lớp khác nhau có thể hiểu cùng một thông điêp theo các cách khác nhau.
+*/
---
+++ Learning-OOP-with-C#/Lesson2_Class/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+/*
+    ?Class trong C#
+        Chính là cách thể hiện khái niệm về lớp trong OOP
+        Một lớp trong C# có các thành phần như thuộc tính, biến, 
+        Phương thức là các hàm thành phần thể hiện hành vi của 1 lớp, Phương thức khởi tạo, phương thức huỷ bỏ
+        Class thực ra là một kiểu dữ liệu mới do người dùng định nghĩa
+
+    ?Khai báo khởi tạo class trong C#
+    class <tên lớp> {
+        <phạm vi truy cập> <Các thành phần của lớp>;
+    }
+    trong đó: 
+    <tên lớp> là tên do người dùng đặt
+    <Phạm vi truy cập> bao gồm các từ khóa như public, protected, private, static,... 
+    <Các thành phàn> bao gồm các biến, phương thức.
+
+    Ví dụ:
+    class Animal{
+        public double Weight;
+        publoc double Height;
+
+        public void Run(){
+            Console.WriteLine("Animal is running...");
+        }
+    }
+    Với khai báo trên ta đã có 1 kiểu dữ liệu mới là Animal, và ta hoàn toàn có thể khai báo và sử dụng nó
+    *Từ khóa public chỉ ra phạm vi truy cập của các thành phần bên trong class
+    Lớp Animal có 2 thuộc tính là Weight, Height và 1 phương thức là Run, như vậy mọi đối tượng thuộc lớp này đều có 3 thành phần trên
+
+
+    ?Phương thức khởi tạo
+    ?Phương thức hủy bỏ
+    ?So sánh class và struct 
+*/
