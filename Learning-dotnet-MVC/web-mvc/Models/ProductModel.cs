3187abed72402ed546af87a7f81678c9ffe9ae77
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-07-06 21:34:11 +0700

    Commit MVC

--- Learning-dotnet-MVC/web-mvc/Models/ProductModel.cs
+++ Learning-dotnet-MVC/web-mvc/Models/ProductModel.cs
@@ -2,7 +2,7 @@ namespace web_mvc
 {
     public class ProductModel{
         public int Id { get; set;}
-        public string Name { get; set;}
+        public string? Name { get; set;}
         public double Price { get; set;}
     }
 }
